cde2e216ecdb5d7479177994716953bbbcce54c0
Sandy Ludosky <sandyludosky@Sandys-MacBook-Pro.local>
2014-06-03 09:57:20 -0400

    Changes added to read me file locally

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ coffeecode1
 
 June 03, 2014
 This is a new read me file
+I am adding some changes to the read me file
--- hello.rb
+++ hello.rb
@@ -1,1 +1,82 @@
 puts "Good Morning World"
+
+=begin
+	
+begin
+	
+rescue 
+# Step One: Creating / Initializing a git repo
+ +
+ +Steps to follow:
+ +
+ +1. Move to your code directory
+ +2. Create a new directory
+ +3. Move into it
+ +4. Create an empty file called "hello.rb"
+ +5. Open the directory you're in with sublime text via the command line
+ +6. Inside the "hello.rb" file, write a Ruby statement that outputs "Good morning". Save your file.
+ +7. Initialize (Create) your git repository
+ +8. Check your current status and read carefully what it says
+ +9. Start tracking / Add the "hello.rb" file to the repository (be specific)
+ +10. Check your current status and read carefully what it says
+ +11. Create your first commit with a message
+ +12. Check your current status and read carefully what it says
+ +13. (BONUS) Check your repository log
+ +
+ +==== 10 mins ====
+ +
+ +If you're done before, go to alexa.bitmakerlabs.com and update your github profile info.
+ +If you're bored, delete your directory and do it all over again. Practice is key here!
+ +
+ +
+ +# Remote repositories
+ +
+ +There are two ways to set up repository on GitHub
+ +
+ +Using an existing local repository and then creating it on github
+ +* OR *
+ +Creating an empty repository on github first and then cloning it locally
+ +
+ +
+ +## Step Two: Using an existing local repository and then creating it on github
+ +1. Create a github repository for the project
+ +2. In your local version, set up the remote repository as "origin". (Follow the instructions on your github page)
+ +3. Check your repository's status, get in the habit of reading
+ +4. Push your project to github
+ +5. Refresh the github page and see your changes have been uploaded to your remote repository
+ +6. Make changes to your "hello.rb" file
+ +7. Check the repo's status
+ +8. Stage your changes
+ +9. Check the repo's status
+ +10. Create a new commit with a meaningful message
+ +11. Check your repo's status
+ +12. Push the code to github (origin)
+ +13. Refresh the github page and see your changes have been uploaded to your remote repository
+ +
+ +== 10 mins ==
+ +
+ +(BONUS)
+ +If you're done:
+ +1. delete the github repo you just created
+ +2. look up how to "unset a remote repository"
+ +3. start all over again!
+ +
+ +## Step Three: Creating an empty repository on github first and then cloning it locally
+ +1. Create a new github repository. Name it whatever you like.
+ +2. Find the clone url (git@...) on the page. You can change it from http
+ +3. Clone the repository
+ +4. Move into the directory
+ +5. Create a new directory inside your project
+ +6. Check the status of the repo - why can't you see the directory you just created?
+ +7. Move into the new directory and create a new empty file called "joke.rb"
+ +8. Open the file with Sublime and have it output "Why did the chicken cross the road?". Save it!
+ +9. Check your repo's status - what's different ?
+ +10. Stage your changes
+ +11. Check the repo's status
+ +12. Create a new commit with a meaningful message
+ +13. Push the code to github (origin)
+ +14. Refresh the github page and see your changes have been uploaded to your remote repository
+  =end
+	
+	
+end
